ca8fe98d7bd97ad310a87f80afdd5db3e2b56320
Marco Cesarato <cesarato.developer@gmail.com>
2021-02-13 23:46:09 +0100

    chore: remove unused methods

--- src/Helper/Formatter.php
+++ src/Helper/Formatter.php
@@ -2,46 +2,15 @@
 
 namespace ConventionalChangelog\Helper;
 
-use DateTime;
-
 class Formatter
 {
     /**
-     * Clean string removing double spaces and trim.
+     * Clean string removing double spaces and trimming.
      */
     public static function clean(string $string): string
     {
         $string = trim($string);
 
         return preg_replace('/[[:blank:]]+/m', ' ', $string);
-    }
-
-    /**
-     *  Get today date string formatted.
-     *
-     * @param DateTime $today
-     */
-    public static function getDateString(DateTime $date): string
-    {
-        $months = [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-            'August',
-            'September',
-            'October',
-            'November',
-            'December',
-        ];
-        $day = date('j', $date->getTimestamp());
-        $month = date('n', $date->getTimestamp());
-        $monthName = $months[$month - 1];
-        $year = date('Y', $date->getTimestamp());
-
-        return $day . ' ' . $monthName . ' ' . $year;
     }
 }
